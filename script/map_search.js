2b89ce804dadd05718b26755e9b31eb2c73e2939
Leesuryeon <lees8351@naver.com>
2026-05-05 18:08:33 +0900

    fix: 오류 수정

--- script/map_search.js
+++ script/map_search.js
@@ -3,6 +3,9 @@ search.addEventListener('click', function(){
     var input_address = document.getElementById('address').value;
     var geocoder = new kakao.maps.services.Geocoder();
     geocoder.addressSearch(input_address, function(result, status){
+        console.log(kakao.maps.services.Status.OK);
+        console.log(status);
+        console.log(result);
         if (status === kakao.maps.services.Status.OK){
             var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
             map.setCenter(coords);
